5985e2413ee80e0af169f44d280ff9000f2d8892
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-04 22:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-const url =  location.href;
+const url = window.location.href;
 let n = url.length;
-alert("The length of the URL is: "+n);
+alert("The length of the URL is: " + n);
